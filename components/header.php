72c8fe2ecea1dc8726cb36dd4b92e62b37400a6a
jelenacavar <jelena.97.cavar@gmail.com>
2017-12-13 22:48:56 +0100

    Photobox feature

--- components/header.php
+++ components/header.php
@@ -18,9 +18,9 @@
             <li data-menuanchor="artists"><a href="index.php#artists">Artists</a></li>
             <li data-menuanchor="about"><a href="index.php#about">About</a></li>
             <li data-menuanchor="gallery"><a href="index.php#gallery">Gallery</a></li>
+            <li data-menuanchor="events"><a href="index.php#events">Events</a></li>
             <li data-menuanchor="photobox"><a href="index.php#photobox">Photobox</a></li>
             <li data-menuanchor="find-us"><a href="index.php#find-us">Find Us</a></li>
-            <li data-menuanchor="events"><a href="events.php">Events</a></li>
             <div class="social-icons-menu">
                 <a href="https://www.instagram.com/kalibaraarhus/?hl=hr"><i class="fa fa-instagram" aria-hidden="true"></i></a>
                 <a href="https://www.facebook.com/KALIBARAARHUS/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 //Home page - one page layout
+//path to the folder that contains the pictures    
+$sti = 'img/photobox/';
+$pictures = array();
+if(file_exists($sti))
+{
+    //Filter "." and ".." from the array and return only the pictures names
+    $pictures = array_filter(scandir($sti,1), 
+                            function ($item) {
+                            return '.' !== $item[0];
+                            }
+                    );    
+}
 ?>
 <!doctype html>
 <html class="no-js" lang="">
@@ -32,7 +44,7 @@
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
                     </div>
-                    
+                        
                 </div>
             </section>
             <section class="section">
@@ -41,11 +53,25 @@
             <section class="section">
                 <!-- Gallery -->
             </section>
-             <section class="section">
+            <section class="section">
                 <!-- Events -->
             </section>
-            <section class="section">
+            <section class="section photobox">
                 <!-- Photobox -->
+                <h1>Photobox</h1>
+                <p>Take a pictures of yourself here at kalibar!</p>
+                <p>We display all the picture below.</p>
+                <i class="fa fa-camera-retro" id="open-ph-modal"></i>
+                <div class="photobox-slider">
+                    <?php foreach($pictures as $key => $picture): ?>
+                        <?php 
+                        // The path of the picture is made by adding the path of the folder 
+                        // Fotorama usage: <a> = Displays picture with full size
+                        // <img> displays the thumbnail of the picture with the defined width and height
+                        ?>
+                    <div><img src="<?php echo $sti . $picture ?>" alt="<?php echo "gallery image N.".$key; ?>"></div>
+                    <?php endforeach; ?>
+                </div>
             </section>
             <section class="section">
                 <!-- Find Us -->
@@ -54,7 +80,15 @@
                 <?php include 'components/footer.php' ?>                       
             </div>
         </main>
-            
+        <!-- Modals -->
+        <div class="modal-container" id="ph-modal" style="display: none;">
+            <i class="modal-close-icon fa fa-times" id="close-ph-modal"></i>
+            <div class="photo-frame" id="video-feed">
+                <video autoplay="false" id="videoElement"></video>                
+                <div class="circle-btn" id="take-picture"></div>
+            </div>
+            <canvas id="photo" style="display: none;"></canvas>
+        </div>
         <?php include 'components/scripts.php'; ?>
         <script src="js/home-fullpage.js"></script>
     </body>
