ac0c56678d260ba8896175acef002f46e7680e60
ankeris <31132643+ankeris@users.noreply.github.com>
2017-12-14 23:31:15 +0100

    Other decision for slide

--- index.php
+++ index.php
@@ -30,28 +30,29 @@ if(file_exists($sti))
                 <!-- Artists -->
                 <div class="artist-container">
                     <div class="artist-details top-middle">
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                        </p>
+                        <img src="img/lars.png" alt="">
+                        <p>Lars Larsen - guide in museum inspired by historical and artistic environment. Since he was 25 years old, he found that the best way to take his emotions away is to shape hand-craft art. Since Lars has more than ten years experience, he is confident enough to share his work at Kalibar!</p>
                     </div>
                     <div class="artist-details bottom-left">
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            Wooden hand-crafts of smaller size sculptures, statues, reflections. High attention to detail, home made, high quality material, inspired by Aarhus and Denmark surroundings art.
                         </p>
                     </div>
                     <div class="artist-details bottom-right">
-                        <div class="slideshow">
-                          <div class="slider slider-1">
-                            <div class="item">
-                              <img src="https://images.unsplash.com/photo-1465935343323-d742334bcbda?crop=entropy&fit=crop&fm=jpg&h=975&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1925">
+                        <div class="wrap">
+                          <div id="arrow-left" class="arrow"></div>
+                          <div id="slider">
+                            <div class="slide slide1">
+
                             </div>
-                            <div class="item">
-                              <img src="https://images.unsplash.com/photo-1443890923422-7819ed4101c0?crop=entropy&fit=crop&fm=jpg&h=975&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1925">
+                            <div class="slide slide2">
+
                             </div>
-                            <div class="item">
-                              <img src="https://images.unsplash.com/photo-1474861644511-0f2775ae97cc?crop=entropy&fit=crop&fm=jpg&h=975&ixjsv=2.1.0&ixlib=rb-0.3.5&q=80&w=1925">
+                            <div class="slide slide3">
+
                             </div>
                           </div>
+                          <div id="arrow-right" class="arrow"></div>
                         </div>
                     </div>
                         
