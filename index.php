8da37cd46d284118d5e348492f6601c19edc37a0
Skywalker190 <31208060+Skywalker190@users.noreply.github.com>
2017-12-15 15:15:11 +0100

    Add
    Home section plus google maps contacts//

--- index.php
+++ index.php
@@ -25,6 +25,21 @@ if(file_exists($sti))
         <main id="fullpage">
             <section class="section">                
                 <!-- Home page -->
+                
+                
+                <div>
+                    <img class="homepic" style="width: 1900px;" src="https://static1.squarespace.com/static/56b1362af8baf3f55713dba7/t/59cabd2aedaed88c86680708/1506458928146/8435293722_d7da782a88_b.jpg?format=1500w" alt="mid">
+                    
+                    <img class="banner" style="max-width: 100%; height: 32%;   position: absolute; top: 55%;" src="banner.png" alt="ban">
+                    
+                    <enter><a href="#" class="button" id="button" style="  color: white; position: absolute; top: 90%; left: 20%;  padding: 25px 50px; background-color: #5ca370; text-decoration: none; border-radius: 8px; transform: translateY(-50%);">More info</a></enter>
+                
+                </div>
+                <!-- Home pgage Ends -->
+                
+                
+                
+                
             </section>
             <section class="artist-section section">
                 <!-- Artists -->
@@ -85,7 +100,21 @@ if(file_exists($sti))
                 </div>
             </section>
             <section class="section">
-                <!-- Find Us -->
+                
+                 
+                <h1 style=" color:white; text-align: center;" >Contact us!</h1>
+                <h2 style=" color: white;text-align: center" >Mobile: 45222222<br>
+                Email: KalibarAarhus@gmail.com</h2>
+                
+                <!-- Find Us Google maps Starts -->
+                
+                <iframe width="100%" height="600px" frameborder="0" style="border:0"
+src="https://www.google.com/maps/embed/v1/search?q=Klostertorvet+5,+8000+Aarhus,+Denmark&key=AIzaSyCorhMR-Y552ZXIrufua-DbnZJETsBjsJI" allowfullscreen></iframe>
+                
+                 <!-- Find Us Google maps Ends -->
+                
+                
+                
             </section>
             <div class="section fp-auto-height">
                 <?php include 'components/footer.php' ?>                       
